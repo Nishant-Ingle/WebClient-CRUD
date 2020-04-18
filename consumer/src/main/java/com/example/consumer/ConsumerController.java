24283ef0382a59d6569f358c9ef02d26b2c0a00f
Nishant Ingle <ningle@vmware.com>
2020-04-18 20:15:44 +0530

    Added Consumer and Producer of WebClient

---
+++ consumer/src/main/java/com/example/consumer/ConsumerController.java
@@ -0,0 +1,78 @@
+package com.example.consumer;
+
+import java.util.HashMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+public class ConsumerController {
+	String baseUrl = "http://localhost:9001";
+	WebClient webClient = WebClient.builder().baseUrl(baseUrl).build();
+
+	@GetMapping(value = "/avengers") 
+	@ResponseBody
+	public Object readAvengers() {
+		return webClient
+				.get()
+				.uri("/avengers")
+				.retrieve()
+				.bodyToMono(HashMap.class)
+				.block();
+	}
+
+	@GetMapping(value = "/avengers/{id}")
+	@ResponseBody
+	public Object readAvenger(@PathVariable String id) {
+		return webClient
+				.get()
+				.uri("/avengers/" + id)
+				.retrieve()
+				.bodyToMono(String.class)
+				.block();
+	}
+
+	@PutMapping(value = "/avengers/{id}")
+	public void updateAvenger(@PathVariable String id, @RequestParam(value = "name") String name) {
+		System.out.println("Passing id: " + id + " , name: " + name);
+		webClient
+		.put()
+		.uri(
+				uriBuilder -> uriBuilder
+				.path("/avengers/" + id)
+				.queryParam("name", name)
+				.build())
+		.exchange()
+		.block();
+	}
+
+	@DeleteMapping(value = "/avengers/{id}")
+	public void deleteAvenger(@PathVariable String id) {
+		webClient
+		.delete()
+		.uri("/avengers/" + id)
+		.exchange()
+		.block();
+	}
+
+	@PostMapping(value = "/avengers")
+	public void createAvengers(@RequestBody HashMap<String, String> requestMap) {
+		System.out.println("Request Map: " + requestMap);
+		webClient
+		.post()
+		.uri("/avengers/")
+		.body(BodyInserters.fromProducer(Mono.just(requestMap), HashMap.class))
+		.exchange()
+		.block();
+	}
+}
---
+++ producer/src/main/java/com/example/producer/ProducerController.java
@@ -0,0 +1,57 @@
+package com.example.producer;
+
+import java.util.HashMap;
+import javax.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProducerController {
+	HashMap<String, String> avengersMap = new HashMap<>();
+
+	@PostConstruct
+	public void initialize() {
+		avengersMap.put("Thor", "Chris Hemsworth");
+		avengersMap.put("Hulk", "Mark Ruffalo");
+		avengersMap.put("Iron Man", "Robert Downey Jr.");
+		avengersMap.put("Captain America", "Chris Evans");
+		avengersMap.put("Black Widow", "Scarlet Johansson");
+		avengersMap.put("Hawkeye", "Jeremy Renner");
+	}
+
+	@GetMapping(value = "/avengers") 
+	@ResponseBody
+	public HashMap<String, String> readAvengers() {
+		return avengersMap;
+	}
+
+	@GetMapping(value = "/avengers/{id}")
+	@ResponseBody
+	public String readAvenger(@PathVariable String id) {
+		return avengersMap.get(id);
+	}
+
+	@PutMapping(value = "/avengers/{id}")
+	public void updateAvenger(@PathVariable String id, @RequestParam(value = "name") String name) {
+		System.out.println("Adding id: " + id + " , name: " + name);
+		avengersMap.put(id, name);
+	}
+
+	@DeleteMapping(value = "/avengers/{id}")
+	public void deleteAvenger(@PathVariable String id) {
+		avengersMap.remove(id);
+	}
+
+	@PostMapping(value = "/avengers")
+	public void createAvengers(@RequestBody HashMap<String, String> requestMap) {
+		System.out.println("Received: " + requestMap);
+		avengersMap = requestMap;
+	}
+}
